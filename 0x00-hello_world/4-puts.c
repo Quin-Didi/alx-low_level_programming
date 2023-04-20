3c3232255f7c6fe0aba89de11688771c6c9192bb
Quin-Didi <ihetuchimmy@gmail.com>
2023-04-20 06:36:01 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Using the function puts to print a line
+ * return: Ensures the code returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
+
