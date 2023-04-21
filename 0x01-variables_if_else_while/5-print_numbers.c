21ca716a4dd3abdb5c272b0f9f091bb560de64ae
Quin-Didi <ihetuchimmy@gmail.com>
2023-04-21 09:17:27 -0700

    bers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints single digits in base 10
+ * putchar - using the for loop
+ * Return: 0
+ */
+int main(void)
+{
+	int bers;
+	while (bers < 10)
+	{
+		putchar(bers + '0');
+		bers++;
+	}
+	putchar('\n');
+	return (0);
+}
